0b5faf8ff5c280d53f2c6c0518cff97b7f28a688
Chris Stott <chrisjstott@gmail.com>
2015-04-15 13:30:30 -0700

    Review rating buttons respond interactively

--- app/assets/javascripts/views/reviews/review_form.js
+++ app/assets/javascripts/views/reviews/review_form.js
@@ -1,15 +1,38 @@
 Lunchin.Views.ReviewForm = Backbone.View.extend({
   template: JST['reviews/form'],
 
-  events: {'submit form.review-form': 'submit'},
+  events: {
+    'submit form.review-form': 'submit',
+    'click .btn-rating-selector': 'selectRating',
+    'mouseenter .btn-rating-selector': 'hoverRating',
+    'mouseleave .btn-rating-selector': 'hoverRating'
+  },
 
   render: function() {
     this.$el.html(this.template);
     return this;
   },
 
-  submit: function() {
+  submit: function(event) {
     event.preventDefault();
+  },
+
+  selectRating: function(event) {
+    event.preventDefault();
+    var rating = $(event.currentTarget).data('rating');
+    $(".btn-rating-selector").removeClass('rating-selected');
+    for (var i = 1; i <= rating; i++) {
+      $(".btn-rating-selector[data-rating='" + i + "']").addClass('rating-selected');
+    }
+    $(".rating-radio-button[value='" + rating + "']").prop('checked', true);
+  },
+
+  hoverRating: function(event) {
+    event.preventDefault();
+    var rating = $(event.currentTarget).data('rating');
+    for (var i = 1; i <= rating; i++) {
+      $(".btn-rating-selector[data-rating='" + i + "']").toggleClass('rating-hover');
+    }
   }
 
 })
