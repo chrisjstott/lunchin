83b76675cb133bb8db40a561fc6ccfea3f860360
Chris Stott <chrisjstott@gmail.com>
2015-05-07 11:47:15 -0700

    link to photos hosted on amazon s3

--- db/seeds.rb
+++ db/seeds.rb
@@ -26,11 +26,11 @@ koz.save
 businesses = Business.create([
   { name: "Koz's Kitchen",
     location: "UN Plaza, McAllister St, San Francisco",
-    owner_id: koz.id, photo_url: 'assets/kozskitchen.jpg'
+    owner_id: koz.id, photo_url: 'https://s3-us-west-1.amazonaws.com/lunchin/assets/kozskitchen.jpg'
   },
-  { name: 'Senor Sisig', location: "1061 Market St, San Francisco", photo_url: 'assets/senorsisig.jpg' },
-  { name: 'Bacon Bacon', location: "135 Polk St, San Francisco", photo_url: 'assets/baconbacon.jpg' },
-  { name: 'KoJa Kitchen', location: "110 5th St, San Francisco", photo_url: 'assets/kojakitchen.jpg' },
-  { name: 'The Chairman', location: "Civic Center Plaza, San Francisco", photo_url: 'assets/thechairman.jpg' },
-  { name: 'RoliRoti', location: "Ferry Building, San Francisco", photo_url: 'assets/roliroti.jpg' },
-  { name: 'Seoul on Wheels', location: "60 Leavenworth St, San Francisco", photo_url: 'assets/seoulonwheels.jpg' } ])
+  { name: 'Senor Sisig', location: "1061 Market St, San Francisco", photo_url: 'https://s3-us-west-1.amazonaws.com/lunchin/assets/senorsisig.jpg' },
+  { name: 'Bacon Bacon', location: "135 Polk St, San Francisco", photo_url: 'https://s3-us-west-1.amazonaws.com/lunchin/assets/baconbacon.jpg' },
+  { name: 'KoJa Kitchen', location: "110 5th St, San Francisco", photo_url: 'https://s3-us-west-1.amazonaws.com/lunchin/assets/kojakitchen.jpg' },
+  { name: 'The Chairman', location: "Civic Center Plaza, San Francisco", photo_url: 'https://s3-us-west-1.amazonaws.com/lunchin/assets/thechairman.jpg' },
+  { name: 'RoliRoti', location: "Ferry Building, San Francisco", photo_url: 'https://s3-us-west-1.amazonaws.com/lunchin/assets/roliroti.jpg' },
+  { name: 'Seoul on Wheels', location: "60 Leavenworth St, San Francisco", photo_url: 'https://s3-us-west-1.amazonaws.com/lunchin/assets/seoulonwheels.jpg' } ])
