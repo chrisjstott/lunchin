cb2bc19f6c641ca3801b4c5f872fbed3f0f5df81
Chris Stott <chrisjstott@gmail.com>
2015-05-30 19:55:34 -0700

    adjust how reviews are displayed

--- app/assets/javascripts/views/reviews/review.js
+++ app/assets/javascripts/views/reviews/review.js
@@ -1,6 +1,7 @@
 Lunchin.Views.Review = Backbone.View.extend({
   template: JST['reviews/show'],
-  tagName: 'div',
+  tagName: 'li',
+  className: 'group',
 
   render: function() {
     var content = this.template({ review: this.model });
